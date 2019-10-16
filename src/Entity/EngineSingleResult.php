e8a086071b63c7133255fdf619f38600db7a7473
Jan Barášek <janbarasek@gmail.com>
2019-10-16 11:24:50 +0200

    Result: Add support for box filtering.

--- src/Entity/EngineSingleResult.php
+++ src/Entity/EngineSingleResult.php
@@ -26,18 +26,25 @@ class EngineSingleResult extends EngineResult
 	private $sources;
 
 	/**
+	 * @var string[]
+	 */
+	private $filters;
+
+	/**
 	 * @param string $query
 	 * @param string $matchedRoute
 	 * @param Box|null $interpret
 	 * @param Box[] $boxes
 	 * @param Source[] $sources
+	 * @param string[] $filters
 	 */
-	public function __construct(string $query, string $matchedRoute, ?Box $interpret = null, array $boxes = [], array $sources = [])
+	public function __construct(string $query, string $matchedRoute, ?Box $interpret = null, array $boxes = [], array $sources = [], array $filters = [])
 	{
 		parent::__construct($query, $matchedRoute);
 		$this->interpret = $interpret;
 		$this->boxes = $boxes;
 		$this->sources = $sources;
+		$this->filters = $filters;
 	}
 
 	/**
@@ -54,6 +61,14 @@ class EngineSingleResult extends EngineResult
 		}
 
 		$return = $withoutNoResult === [] ? $this->boxes : $withoutNoResult;
+
+		if ($this->filters !== []) {
+			foreach ($return as $boxKey => $box) {
+				if (\in_array($box->getTag(), $this->filters, true) === false) {
+					unset($return[$boxKey]);
+				}
+			}
+		}
 
 		usort($return, static function (Box $a, Box $b): int {
 			return $a->getRank() < $b->getRank() ? 1 : -1;
