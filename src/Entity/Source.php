bfd8683be1591d04898d3a6ccf95db8d221ed291
Jan Barášek <janbarasek@gmail.com>
2020-05-02 20:23:01 +0200

    Source: Refactoring of rendering function + better support for HTML escaping + tests

--- src/Entity/Source.php
+++ src/Entity/Source.php
@@ -41,81 +41,105 @@ final class Source
 	/**
 	 * @return string
 	 */
-	public function __toString(): string
+	public function render(): string
 	{
-		$title = null;
-		$description = null;
+		$return = [];
 
 		if ($this->title) {
-			$title = '<b>' . $this->title . '</b><br>';
+			$title = htmlspecialchars($this->title, ENT_NOQUOTES | ENT_SUBSTITUTE, 'UTF-8');
+
+			if ($this->url !== null && Validators::isUrl($this->url)) {
+				$return[] = '<a href="' . $this->url . '" target="_blank">' . $title . '</a>';
+			} else {
+				$return[] = '<b>' . $title . '</b>';
+			}
 		}
 
 		if ($this->description) {
-			$description = $this->description
-				. ($this->authors !== []
-					? '<br><br><span class="text-secondary">- ' . implode('<br>- ', $this->authors) . '</span>'
-					: ''
-				);
+			$return[] = htmlspecialchars($this->description, ENT_NOQUOTES | ENT_SUBSTITUTE, 'UTF-8');
 		}
 
-		if ($this->url !== null && Validators::isUrl($this->url)) {
-			return '<a href="' . $this->url . '" target="_blank">'
-				. ($title ?? $this->url)
-				. '</a>'
-				. ($description ?? '');
+		if ($this->authors !== []) {
+			$authors = [];
+
+			foreach ($this->authors as $author) {
+				$authors[] = htmlspecialchars($author, ENT_NOQUOTES | ENT_SUBSTITUTE, 'UTF-8');
+			}
+
+			$return[] = 'Authors:<br><span class="text-secondary">- ' . implode('<br>- ', $authors) . '</span>';
 		}
 
-		return $title === null && $description === null
-			? ''
-			: '<b>' . $title . '</b><br>' . $description;
+		return implode('<br>', $return);
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function __toString(): string
+	{
+		return $this->render();
 	}
 
 
 	/**
 	 * @param string $author
+	 * @return Source
 	 */
-	public function setAuthor(string $author): void
+	public function addAuthor(string $author): self
 	{
 		$this->authors[] = $author;
+
+		return $this;
 	}
 
 
 	/**
 	 * @param string[] $authors
+	 * @return Source
 	 */
-	public function setAuthors(array $authors): void
+	public function setAuthors(array $authors): self
 	{
-		if ($authors !== []) {
-			foreach ($authors as $author) {
-				$this->authors[] = $author;
-			}
+		foreach ($authors as $author) {
+			$this->addAuthor($author);
 		}
+
+		return $this;
 	}
 
 
 	/**
 	 * @param string $url
+	 * @return Source
 	 */
-	public function setUrl(string $url): void
+	public function setUrl(string $url): self
 	{
 		$this->url = $url;
+
+		return $this;
 	}
 
 
 	/**
 	 * @param string $title
+	 * @return Source
 	 */
-	public function setTitle(string $title): void
+	public function setTitle(string $title): self
 	{
 		$this->title = $title;
+
+		return $this;
 	}
 
 
 	/**
 	 * @param string $description
+	 * @return Source
 	 */
-	public function setDescription(string $description): void
+	public function setDescription(string $description): self
 	{
 		$this->description = $description;
+
+		return $this;
 	}
 }
